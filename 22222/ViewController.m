15b43fd1fbaaddd8367a2909f1a85baf60ebab7b
王云 <wangyun120411@163.com>
2017-05-13 17:32:49 +0800

    提交

--- 22222/ViewController.m
+++ 22222/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     UIView *view = [[UIView alloc]init];
+    [self.view addSubview:view];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
